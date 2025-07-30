adbe7c6e89f14369cd34b03bc6db1704bdb7b674
Sara <mesasg17@gmail.com>
2025-07-30 18:28:59 -0500

    Second commit

---
+++ src/main/java/com/shopchop/shopchop/ShopchopApplication.java
@@ -0,0 +1,13 @@
+package com.shopchop.shopchop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopchopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopchopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopchop/shopchop/ShopchopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopchop.shopchop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopchopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
