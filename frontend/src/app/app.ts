76a74938768982464304d9b3759248e68ec8cb36
Lizbethhespinosa <160246869+Lizbethhespinosa@users.noreply.github.com>
2025-08-07 21:23:23 -0500

    Front corrections

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,36 +2,27 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs';
+// 1. Importa CommonModule aquí
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  // 2. Añade CommonModule a la lista de imports
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  searchQuery = '';
-  isLoggedIn = false;
+  showHeader: boolean = true;
+  isLoggedIn: boolean = false; 
+  cartCount= 0;
   protected readonly title = signal('angularSC');
-  itsMain = false;
-  cartCount = 0;
 
-  constructor(private router: Router){
+  constructor(private router: Router) {
     this.router.events
-    .pipe(filter(event =>event instanceof NavigationEnd))
-    .subscribe((event: NavigationEnd) => {
-      this.itsMain = event.urlAfterRedirects === '/';
-    })
-  }
-
-  goToLogIn(){
-    this.router.navigate(["src\app\log-in\log-in.html"]);
-  }
-  logout() {
-    console.log('Cerrar sesión');
-  }
-
-  showCart() {
-    console.log('Mostrar carrito');
-  }
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.showHeader = !event.urlAfterRedirects.includes('/log-in') && !event.urlAfterRedirects.includes('/register');
+      });
+  }
 }
